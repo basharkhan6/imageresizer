9c53384b1047c4aca196aac566122da938d1d566
Bashar <basharkhan6@gmail.com>
2019-06-27 05:19:16 +0600

    beta 0.13 Scale option added Bootstrap Design

--- action.php
+++ action.php
@@ -4,7 +4,7 @@ require "SimpleImage.php";
 $message = "";
 
 $target_dir = "uploads/";
-$target_file = $target_dir . rand(10000, 90000) . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . ImageResizer.tk . rand(10000, 90000) . basename($_FILES["fileToUpload"]["name"]);
 $imageOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
@@ -38,7 +38,7 @@ if ($imageOk == 0) {
     $message = "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    $fileName = store_uploaded_image("fileToUpload", 400, 500, $target_file);
+    $fileName = store_uploaded_image("fileToUpload", 400, 500, $target_file, $_POST["scale"]);
     $message = "Successfully Resized.";
     $message .= '<a href="http://imageresizer.tk/' . $fileName . '">see</a>';
 }
@@ -46,11 +46,12 @@ if ($imageOk == 0) {
 
 echo $message;
 
-function store_uploaded_image($html_element_name, $new_img_width, $new_img_height, $target_file) {
+function store_uploaded_image($html_element_name, $new_img_width, $new_img_height, $target_file, $scale) {
 
     $image = new SimpleImage();
     $image->load($_FILES[$html_element_name]['tmp_name']);
-    $image->resize($new_img_width, $new_img_height);
+    $image->scale();
+    // $image->resize($new_img_width, $new_img_height);
     $image->save($target_file);
     return $target_file; //return name of saved file in case you want to store it in you database or show confirmation message to user
 
